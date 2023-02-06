b320cda1171a967cc9dcec9a5fda387d5af15cd6
Bernard Mawulorm Kofi Wodoame <98283377+wodoame@users.noreply.github.com>
2023-02-06 02:47:14 -0800

    Update and rename QuerySet.extend().py to join_querysets.py

--- QuerySet.extend().py
+++
@@ -1,7 +0,0 @@
-queryset1 = Model.objects.filter(field1='value1')
-queryset2 = Model.objects.filter(field2='value2')
-
-# Use the extend method to add queryset2 to the end of queryset1
-queryset1.extend(queryset2)
-
-#Note that extend modifies the queryset in place and returns None, it's similar to list.extend().
---
+++ join_querysets.py
@@ -0,0 +1,5 @@
+queryset1 = Model.objects.filter(field1='value1')
+queryset2 = Model.objects.filter(field2='value2')
+
+# Use the '|' operator to create a new queryset that contains elements from both querysets
+combined_queryset = queryset1 | queryset2
