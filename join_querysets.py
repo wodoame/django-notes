8ce47aeaacc17bba2a774b10a9908b7e35d0ab68
Bernard Mawulorm Kofi Wodoame <98283377+wodoame@users.noreply.github.com>
2023-10-12 13:51:09 +0000

    Update join_querysets.py

--- join_querysets.py
+++ join_querysets.py
@@ -3,3 +3,4 @@ queryset2 = Model.objects.filter(field2='value2')
 
 # Use the '|' operator to create a new queryset that contains elements from both querysets
 combined_queryset = queryset1 | queryset2
+# Since a queryset is a set there are no duplicates.
